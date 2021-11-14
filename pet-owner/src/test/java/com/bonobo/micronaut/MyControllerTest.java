f79ef17dfce008a45c284c55e6ee6c3ae106e1df
Arde <c_burns93@yahoo.ca>
2021-11-14 11:53:41 +0100

    simple test

---
+++ pet-owner/src/test/java/com/bonobo/micronaut/MyControllerTest.java
@@ -0,0 +1,39 @@
+package com.bonobo.micronaut;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest
+public class MyControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+//    @Test
+//    public void supplyAnInvalidOrderTriggersValidationFailure() {
+//        HttpClientResponseException thrown = assertThrows(HttpClientResponseException.class, () -> {
+//            client.toBlocking().exchange(HttpRequest.GET("/genres/list?order=foo"));
+//        });
+//
+//        assertNotNull(thrown.getResponse());
+//        assertEquals(HttpStatus.BAD_REQUEST, thrown.getStatus());
+//    }
+
+    @Test
+    public void testIndex() {
+        HttpRequest<String> request = HttpRequest.GET("/");
+        String body = this.client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hello World", body);
+    }
+}
