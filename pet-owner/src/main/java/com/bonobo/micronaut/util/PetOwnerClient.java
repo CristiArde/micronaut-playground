15c5bc410793254b1d85903f1d3f2825d1d531f1
Arde <c_burns93@yahoo.ca>
2021-11-19 19:55:40 +0100

    added remaining services for the petownerclient

--- pet-owner/src/main/java/com/bonobo/micronaut/util/PetOwnerClient.java
+++ pet-owner/src/main/java/com/bonobo/micronaut/util/PetOwnerClient.java
@@ -1,7 +1,12 @@
 package com.bonobo.micronaut.util;
 
 import com.bonobo.micronaut.domain.Owner;
+import com.bonobo.micronaut.domain.Pet;
+import com.bonobo.micronaut.domain.Visit;
 import com.bonobo.micronaut.service.impl.OwnerService;
+import com.bonobo.micronaut.service.impl.PetService;
+import com.bonobo.micronaut.service.impl.PetTypeService;
+import com.bonobo.micronaut.service.impl.VisitService;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
@@ -9,7 +14,10 @@ import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 
 @Singleton
@@ -17,13 +25,22 @@ public class PetOwnerClient {
     private final Logger log = LoggerFactory.getLogger(PetOwnerClient.class);
 
     private final OwnerService ownerService;
+    private final PetService petService;
+    private final VisitService visitService;
+    private final PetTypeService petTypeService;
 
-
-    public PetOwnerClient(OwnerService ownerService) {
+    public PetOwnerClient(OwnerService ownerService, PetService petService, VisitService visitService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
+        this.petService = petService;
+        this.visitService = visitService;
+        this.petTypeService = petTypeService;
     }
 
     public void performDatabaseOperations() {
+        performFindAll();
+        Owner owner = performSave();
+        performFindOne(owner.getId());
+        performDelete(owner);
         performFindAll();
     }
 
@@ -37,6 +54,68 @@ public class PetOwnerClient {
             List<Owner> owners = pOwners.getContent();
             owners.forEach(owner -> log.info("Owner: {}, {}", owner.getFirstName(), owner.getLastName()));
         }
+    }
+
+    protected void performFindOne(Long id) {
+        log.info("-----------------------------------------------------");
+        log.info("Request to performFindOne for id: {}", id);
+        log.info("-----------------------------------------------------");
+        Optional<Owner> oOwner = ownerService.findOne(id);
+        oOwner.ifPresent(owner -> log.info("Owner: {}, {}", owner.getFirstName(), owner.getLastName()));
+    }
+
+    protected Owner performSave() {
+        Owner owner = initOwner();
+        log.info("-----------------------------------------------------");
+        log.info("Request to performSave for owner: {}", owner);
+        log.info("-----------------------------------------------------");
+        return ownerService.save(owner);
+    }
+
+    protected void performDelete(Owner owner) {
+        log.info("------------------------------------------------------");
+        log.info("Request to performDelete for owner: {}", owner);
+        log.info("-------------------------------------------------------");
+
+        /** delete owner pets and their visits */
+        Set<Pet> pets = owner.getPets();
+
+        if (CollectionUtils.isNotEmpty(pets)) {
+            for (Pet pet : pets) {
+                Set<Visit> visits = pet.getVisits();
+                if (CollectionUtils.isNotEmpty(visits)) {
+                    for (Visit visit : visits) {
+                        visitService.delete(visit.getId());
+                    }
+                }
+                petService.delete(pet.getId());
+            }
+        }
+
+        ownerService.delete(owner.getId());
+    }
+
+    private Owner initOwner() {
+        Owner owner = new Owner();
+
+        owner.setFirstName("Foo");
+        owner.setLastName("Bar");
+        owner.setCity("Toronto");
+        owner.setAddress("404 Adelaide St W");
+        owner.setTelephone("647000999");
+
+        Pet pet = new Pet();
+        pet.setType(petTypeService.findAll(Pageable.unpaged()).getContent().get(1));
+        pet.setName("Baz");
+        pet.setBirthDate(LocalDate.of(2010, 12, 12));
+        pet.setOwner(owner);
+
+        Visit visit = new Visit();
+        visit.setVisitDate(LocalDate.now());
+        visit.setDescription("Breathing issues");
+        visit.setPet(pet);
+
+        return owner;
     }
 
 }
