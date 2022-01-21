33f5b95c9958fd485e642a1613384c7085e809b1
Arde <c_burns93@yahoo.ca>
2022-01-21 19:44:08 +0100

    created request error exception using the io.micronaut.problem:micronaut-problem-json package

---
+++ pet-owner/src/main/java/com/bonobo/micronaut/web/rest/errors/ErrorConstants.java
@@ -0,0 +1,10 @@
+package com.bonobo.micronaut.web.rest.errors;
+
+import java.net.URI;
+
+public class ErrorConstants {
+    public static final URI DEFAULT_TYPE = URI.create("about:blank");
+
+    private ErrorConstants() {
+    }
+}
