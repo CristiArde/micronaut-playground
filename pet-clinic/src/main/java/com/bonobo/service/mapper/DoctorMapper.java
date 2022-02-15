6f677e75066364ca69ee9b1879257838051367dc
Arde <c_burns93@yahoo.ca>
2022-02-16 00:14:04 +0100

    mappers for doctor and specilitz dto and entity

--- pet-clinic/src/main/java/com/bonobo/service/mapper/DoctorMapper.java
+++ pet-clinic/src/main/java/com/bonobo/service/mapper/DoctorMapper.java
@@ -1,2 +1,10 @@
-package com.bonobo.service.mapper;public class DoctorMapper {
+package com.bonobo.service.mapper;
+
+import com.bonobo.domain.Doctor;
+import com.bonobo.service.dto.DoctorDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "jsr330", uses = {SpecialtyMapper.class})
+public interface DoctorMapper extends EntityMapper<DoctorDto, Doctor> {
+
 }
--- pet-clinic/src/main/java/com/bonobo/service/mapper/SpecialtyMapper.java
+++ pet-clinic/src/main/java/com/bonobo/service/mapper/SpecialtyMapper.java
@@ -1,2 +1,10 @@
-package com.bonobo.service.mapper;public interface SpecialtyMapper {
+package com.bonobo.service.mapper;
+
+import com.bonobo.domain.Specialty;
+import com.bonobo.service.dto.SpecialtyDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "jsr330", uses = {})
+public interface SpecialtyMapper extends EntityMapper<SpecialtyDto, Specialty> {
+
 }
